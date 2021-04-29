4261ff7944ed8561acab8bdf02a02585095b0ef3
justsoph <sophiaanaklumythic@gmail.com>
2021-04-30 06:37:10 +0900

    Plugins de Notificação e de Vibração.

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,2 +1,22 @@
-// This is a JavaScript file
+$(document).on("click", "#alerta", function(){
+  navigator.notification.alert("Minha mensagem.", null, "Aviso!", "Aceito");
+});
 
+$(document).on("click", "#confirm", function(){
+  function confirma(buttonIndex){
+    if(buttonIndex == 1){
+      navigator.notification.alert("Escolheu a opção A");
+    }else if(buttonIndex == 2){
+      navigator.notification.alert("Escolheu a opção B");
+    }
+  }
+  navigator.notification.confirm("Escolha A ou B.", confirma, "Escolha", ["A", "B"]);
+});
+
+$(document).on("click", "#beep", function(){
+  navigator.notification.beep(3);
+});
+
+$(document).on("click", "#vibrar", function(){
+  navigator.vibrate(3000);
+});
